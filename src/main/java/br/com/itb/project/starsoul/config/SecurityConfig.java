a999ace00aaac978013c5828fd006edc48d8aa95
Pedro Henrique <pedropalermo.dev@gmail.com>
2025-09-17 11:24:10 -0300

    Update SecurityConfig.java
    Resolução de bugs

--- src/main/java/br/com/itb/project/starsoul/config/SecurityConfig.java
+++ src/main/java/br/com/itb/project/starsoul/config/SecurityConfig.java
@@ -50,11 +50,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173" , "https://starsoul.netlify.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
-
+    
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
